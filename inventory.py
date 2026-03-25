e681334a6ea5ec566dbad75144045e43275aa1ff
Silvio Barrios <56333049+silvio1615@users.noreply.github.com>
2026-03-25 10:16:44 -0500

    first commit

---
+++ inventory.py
@@ -0,0 +1,6 @@
+producto = input("Ingrese nombre del producto: ")
+
+cantidad = int(input("Ingrese la cantidad: "))
+precio = float(input("Ingrese el precio x unidad: "))
+costo_total=precio*cantidad
+print(f"producto: {producto},cantidad: {cantidad},precio: {precio},costo total: {costo_total}")
